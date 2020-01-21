6c635bb226d71e70219b0720ead25f81c6bc7d44
JonFillip <tjayphil14@gmail.com>
2020-01-21 11:22:41 +0200

    Updated file path in knight and bullet scripts

--- bullet.py
+++ bullet.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.sprite import Sprite
+from AlienApocalypse.knight import Mage
 
 
 class Bullet(Sprite):
@@ -8,11 +9,16 @@ class Bullet(Sprite):
     def __init__(self, ap_game):
         """Create a bullet object at the mage's current position."""
         super().__init__()
+
+        # Initialize the mage attributes and instances in bullet class
+        self.mage = Mage(self)
+        self.last_move = None
+
         self.bullet_image_right = [
-            pygame.image.load("/Users/johnphillip/Desktop/alien_apocalypse/craftpix-891165-assassin-mage-viking-free"
-                              "-pixel-art-game-heroes/Mage/Fire_Extra/fire_right1.png"),
-            pygame.image.load("/Users/johnphillip/Desktop/alien_apocalypse/craftpix-891165-assassin-mage-viking-free"
-                              "-pixel-art-game-heroes/Mage/Fire_Extra/fire_right2.png"),
+            pygame.image.load("/Users/johnphillip/Desktop/alien_apocalypse/AlienApocalypse/craftpix-891165-assassin"
+                              "-mage-viking-free-pixel-art-game-heroes/Mage/Fire_Extra/fire_right1.png"),
+            pygame.image.load("/Users/johnphillip/Desktop/alien_apocalypse/AlienApocalypse/craftpix-891165-assassin"
+                              "-mage-viking-free-pixel-art-game-heroes/Mage/Fire_Extra/fire_right2.png"),
             pygame.image.load("/Users/johnphillip/Desktop/alien_apocalypse/craftpix-891165-assassin-mage-viking-free"
                               "-pixel-art-game-heroes/Mage/Fire_Extra/fire_right3.png"),
             pygame.image.load("/Users/johnphillip/Desktop/alien_apocalypse/craftpix-891165-assassin-mage-viking-free"
@@ -54,7 +60,6 @@ class Bullet(Sprite):
 
         # Create a bullet rect at (0, 0) and then set the correct position
         self.rect = pygame.Rect(0, 0, self.settings.bullet_width, self.settings.bullet_height, self.rect)
-        self.rect.centerx = ap_game.mage.rect.centerx
 
         # Store the bullet's position as a decimal value.
         self.x = float(self.rect.x)
@@ -67,4 +72,9 @@ class Bullet(Sprite):
 
     def draw_bullet(self):
         """Draw the bullet to the screen."""
-        pygame.draw.rect(self.screen, self.bullet_image_right, self.rect)
+        if self.last_move == self.mage.moving_right:
+            for bullet in self.bullet_image_right:
+                self.screen.blit(self.screen, self.bullet_image_right, self.rect, bullet[1], 0, 0)
+        elif self.last_move == self.mage.moving_left:
+            for bullet in self.bullet_image_left:
+                self.screen.blit(self.screen, self.bullet_image_left, self.rect, bullet[1], 0, 0)
--- knight.py
+++ knight.py
@@ -13,36 +13,37 @@ class Mage:
 
         # Load images for character walking movement
         self.walk_left = [
-            pygame.image.load("/Users/johnphillip/Desktop/alien_apocalypse/craftpix-891165-assassin-mage-viking-free"
-                              "-pixel-art-game-heroes/Mage/Walk/left_walk1.bmp"),
-            pygame.image.load("/Users/johnphillip/Desktop/alien_apocalypse/craftpix-891165-assassin-mage-viking-free"
-                              "-pixel-art-game-heroes/Mage/Walk/left_walk2.bmp"),
-            pygame.image.load("/Users/johnphillip/Desktop/alien_apocalypse/craftpix-891165-assassin-mage-viking-free"
-                              "-pixel-art-game-heroes/Mage/Walk/left_walk3.bmp"),
-            pygame.image.load("/Users/johnphillip/Desktop/alien_apocalypse/craftpix-891165-assassin-mage-viking-free"
-                              "-pixel-art-game-heroes/Mage/Walk/left_walk4.bmp"),
-            pygame.image.load("/Users/johnphillip/Desktop/alien_apocalypse/craftpix-891165-assassin-mage-viking-free"
-                              "-pixel-art-game-heroes/Mage/Walk/left_walk5.bmp"),
-            pygame.image.load("/Users/johnphillip/Desktop/alien_apocalypse/craftpix-891165-assassin-mage-viking-free"
-                              "-pixel-art-game-heroes/Mage/Walk/left_walk6.bmp")
+            pygame.image.load("/Users/johnphillip/Desktop/alien_apocalypse/AlienApocalypse/craftpix-891165-assassin"
+                              "-mage-viking-free-pixel-art-game-heroes/Mage/Walk/left_walk1.bmp"),
+            pygame.image.load("/Users/johnphillip/Desktop/alien_apocalypse/AlienApocalypse/craftpix-891165-assassin"
+                              "-mage-viking-free-pixel-art-game-heroes/Mage/Walk/left_walk2.bmp"),
+            pygame.image.load("/Users/johnphillip/Desktop/alien_apocalypse/AlienApocalypse/craftpix-891165-assassin"
+                              "-mage-viking-free-pixel-art-game-heroes/Mage/Walk/left_walk3.bmp"),
+            pygame.image.load("/Users/johnphillip/Desktop/alien_apocalypse/AlienApocalypse/craftpix-891165-assassin"
+                              "-mage-viking-free-pixel-art-game-heroes/Mage/Walk/left_walk4.bmp"),
+            pygame.image.load("/Users/johnphillip/Desktop/alien_apocalypse/AlienApocalypse/craftpix-891165-assassin"
+                              "-mage-viking-free-pixel-art-game-heroes/Mage/Walk/left_walk5.bmp"),
+            pygame.image.load("/Users/johnphillip/Desktop/alien_apocalypse/AlienApocalypse/craftpix-891165-assassin"
+                              "-mage-viking-free-pixel-art-game-heroes/Mage/Walk/left_walk6.bmp")
         ]
         self.walk_right = [
-            pygame.image.load("/Users/johnphillip/Desktop/alien_apocalypse/craftpix-891165-assassin-mage-viking-free"
-                              "-pixel-art-game-heroes/Mage/Walk/walk1.bmp"),
-            pygame.image.load("/Users/johnphillip/Desktop/alien_apocalypse/craftpix-891165-assassin-mage-viking-free"
-                              "-pixel-art-game-heroes/Mage/Walk/walk2.bmp"),
-            pygame.image.load("/Users/johnphillip/Desktop/alien_apocalypse/craftpix-891165-assassin-mage-viking-free"
-                              "-pixel-art-game-heroes/Mage/Walk/walk3.bmp"),
-            pygame.image.load("/Users/johnphillip/Desktop/alien_apocalypse/craftpix-891165-assassin-mage-viking-free"
-                              "-pixel-art-game-heroes/Mage/Walk/walk4.bmp"),
-            pygame.image.load("/Users/johnphillip/Desktop/alien_apocalypse/craftpix-891165-assassin-mage-viking-free"
-                              "-pixel-art-game-heroes/Mage/Walk/walk5.bmp"),
-            pygame.image.load("/Users/johnphillip/Desktop/alien_apocalypse/craftpix-891165-assassin-mage-viking-free"
-                              "-pixel-art-game-heroes/Mage/Walk/walk6.bmp")
+            pygame.image.load("/Users/johnphillip/Desktop/alien_apocalypse/AlienApocalypse/craftpix-891165-assassin"
+                              "-mage-viking-free-pixel-art-game-heroes/Mage/Walk/walk1.bmp"),
+            pygame.image.load("/Users/johnphillip/Desktop/alien_apocalypse/AlienApocalypse/craftpix-891165-assassin"
+                              "-mage-viking-free-pixel-art-game-heroes/Mage/Walk/walk2.bmp"),
+            pygame.image.load("/Users/johnphillip/Desktop/alien_apocalypse/AlienApocalypse/craftpix-891165-assassin"
+                              "-mage-viking-free-pixel-art-game-heroes/Mage/Walk/walk3.bmp"),
+            pygame.image.load("/Users/johnphillip/Desktop/alien_apocalypse/AlienApocalypse/craftpix-891165-assassin"
+                              "-mage-viking-free-pixel-art-game-heroes/Mage/Walk/walk4.bmp"),
+            pygame.image.load("/Users/johnphillip/Desktop/alien_apocalypse/AlienApocalypse/craftpix-891165-assassin"
+                              "-mage-viking-free-pixel-art-game-heroes/Mage/Walk/walk5.bmp"),
+            pygame.image.load("/Users/johnphillip/Desktop/alien_apocalypse/AlienApocalypse/craftpix-891165-assassin"
+                              "-mage-viking-free-pixel-art-game-heroes/Mage/Walk/walk6.bmp")
         ]
 
         # Load the character - mage mage and get its rect.
-        image_file = "/craftpix-891165-assassin-mage-viking-free-pixel-art-game-heroes/Mage/Walk/standing.bmp"
+        image_file = "/Users/johnphillip/Desktop/alien_apocalypse/AlienApocalypse/craftpix-891165-assassin-mage" \
+                     "-viking-free-pixel-art-game-heroes/Mage/Walk/standing.bmp"
 
         self.mage = pygame.image.load(image_file)
         self.rect = self.mage.get_rect()
